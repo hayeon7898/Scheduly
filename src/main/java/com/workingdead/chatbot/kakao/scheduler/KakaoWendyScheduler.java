db185cadb79551ced70d7c858d8d83feff7dd371
hayeon7898 <hayeon22@korea.ac.kr>
2026-05-06 10:37:43 +0900

    chore: add logging to verify scheduler recovery

--- src/main/java/com/workingdead/chatbot/kakao/scheduler/KakaoWendyScheduler.java
+++ src/main/java/com/workingdead/chatbot/kakao/scheduler/KakaoWendyScheduler.java
@@ -54,6 +54,7 @@ public class KakaoWendyScheduler {
                 continue;
             }
             restoreSchedule(vote, botGroupKey);
+            log.info("[복구 확인] botGroupKey={}, sessionVoteId={}", botGroupKey, sessionVoteId.get(botGroupKey));
             // 인메모리 맵 복구
             kakaoWendyService.restoreVoteMapping(vote.getId(), botGroupKey);
         }
